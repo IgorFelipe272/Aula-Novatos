db13a653d440287ebfbbefe534f75e2354d40f75
Amanda Arantes <amandaaalmeida0609@gmail.com>
2025-05-31 01:02:00 -0300

    script de movimentacao

---
+++ Assets/AA_Script/movimentacao.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class movimentacao : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (rb == null)
+            {
+            Debug.Log("nao tem Rigidbody2D");
+            }
+    }
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        moveInput.x = moveInput.normalized;
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (rb == null)
+{
+    Debug.Log("N tem Rigidbody2D");
+}
