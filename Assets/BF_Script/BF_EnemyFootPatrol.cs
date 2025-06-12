0b707472ee9cba08b90138c35a228bc572c67a29
B Florenzano <bevelar01@gmail.com>
2025-06-12 00:52:04 -0300

    ADD [prebab Rino] ATT [BF_enemyFootPatrol]

--- Assets/BF_Script/BF_EnemyFootPatrol.cs
+++ Assets/BF_Script/BF_EnemyFootPatrol.cs
@@ -8,7 +8,9 @@ public class BF_EnemyFootPatrol : MonoBehaviour
     public LayerMask groundLayer;
 
     private bool isGrounded = true;
-    private Vector2 moveDirection = Vector2.right;
+    private Vector2 moveDirection = Vector2.left;
+
+    private int numCollisions = 0;
 
     private void Start()
     {
@@ -25,23 +27,47 @@ public class BF_EnemyFootPatrol : MonoBehaviour
         enemyRb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, enemyRb.linearVelocity.y);
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Se colidiu com um chão, verificar se deve mudar de direcao
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            // Adiciona um no contador de colisões
+            numCollisions = numCollisions + 1;
+            if (numCollisions >= 2)
+            {
+                Flip();
+            }
+            //Debug.Log("enter " + numCollisions);
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         // Se saiu do ch�o, inverter dire��o
         if (((1 << collision.gameObject.layer) & groundLayer) != 0)
         {
-            Flip();
+            // Remove um no contador de colisões
+            numCollisions = numCollisions - 1;
+
+            // Se não colide com nada
+            if(numCollisions == 0)
+            {
+                Flip();
+            }
+            //Debug.Log("exit " + numCollisions);
         }
     }
 
     private void Flip()
     {
         // Inverter dire��o de movimento
-        moveDirection = -1;
+        moveDirection *= -1;
 
         // Flipar visualmente (espelhar) o inimigo
         Vector3 scale = enemyTransform.localScale;
-        scale.x= -1;
+        scale.x *= -1;
         enemyTransform.localScale = scale;
     }
 }
