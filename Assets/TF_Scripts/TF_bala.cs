d9cec965a95076d8ff09f0a630e84bbde1a281f0
Kyetsu <thiago.falcaorosi@gmail.com>
2025-07-11 15:25:24 -0300

    AAAAA
    A

---
+++ Assets/TF_Scripts/TF_bala.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TF_bala : MonoBehaviour
+{
+
+    public int dano = 1;
+    public float tempoDeVida = 5f;
+    void Start()
+    {
+
+        Destroy(gameObject, tempoDeVida);  
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            TF_healthSys alvo = collision.gameObject.GetComponent<TF_healthSys>();
+            if (alvo != null)
+            {
+                alvo.takeDamage(dano);
+            }
+        }
+       
+            Destroy(gameObject);
+        
+
+       
+        
+    }
+    
+        
+    
+}
